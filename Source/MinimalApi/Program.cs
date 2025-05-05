e70fdfb43884241582d888cc6afbc151aa048f74
unknown <170903318+NathanGitCode@users.noreply.github.com>
2025-05-05 14:17:42 +1200

    Initial working Dockerfile

--- Source/MinimalApi/Program.cs
+++ Source/MinimalApi/Program.cs
@@ -5,6 +5,7 @@ namespace MinimalApi
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
             var app = builder.Build();
 
             app.MapGet("/", () => "Hello World!");
